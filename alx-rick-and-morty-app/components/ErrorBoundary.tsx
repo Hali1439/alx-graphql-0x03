5dc3f03b57a50d732a7eb35f3cf3773970a1a1b8
Halima Muktar <halimamuk1307@gmail.com>
2025-08-20 02:42:47 +0300

    Adding Error Prone Component

--- alx-rick-and-morty-app/components/ErrorBoundary.tsx
+++ alx-rick-and-morty-app/components/ErrorBoundary.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React, { Component, ErrorInfo, ReactNode } from 'react';
 
 interface State {
   hasError: boolean;
@@ -8,36 +8,32 @@ interface ErrorBoundaryProps {
   children: ReactNode;
 }
 
-class ErrorBoundary extends React.Component<ErrorBoundaryProps, State> {
+class ErrorBoundary extends Component<ErrorBoundaryProps, State> {
   constructor(props: ErrorBoundaryProps) {
     super(props);
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(_: Error): State {
+  static getDerivedStateFromError(error: Error): State {
     return { hasError: true };
   }
 
   componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
-    console.log("ErrorBoundary caught an error", { error, errorInfo });
+    console.log({ error, errorInfo });
   }
 
   render() {
     if (this.state.hasError) {
       return (
-        <div className="p-4 text-center">
-          <h2 className="text-red-600 font-bold text-lg">
-            Oops, there is an error!
-          </h2>
-          <button
-            className="mt-2 px-4 py-2 bg-blue-600 text-white rounded-lg"
-            onClick={() => this.setState({ hasError: false })}
-          >
+        <div>
+          <h2>Oops, there is an error!</h2>
+          <button onClick={() => this.setState({ hasError: false })}>
             Try again?
           </button>
         </div>
       );
     }
+
     return this.props.children;
   }
 }
